c13f7dff059d59d0f183284a90067c4f2dc3d9e1
g-akca <gok.akca66@gmail.com>
2026-05-14 00:33:23 +0300

    Fix error display

--- src/components/InputDiv.jsx
+++ src/components/InputDiv.jsx
@@ -9,6 +9,8 @@ function InputDiv({
 }) {
   const hasError = error || sharedDateError;
 
+  const shouldShowError = error && (!sharedDateError || (sharedDateError && showDateMessage));
+
   return (
     <div className="flex flex-col gap-2">
       <label 
@@ -25,7 +27,7 @@ function InputDiv({
         className={`border rounded-lg px-4 py-2 font-bold caret-purple-500 placeholder:text-black/50 focus:outline-none tablet:px-6 tablet:text-[32px] tablet:leading-base ${hasError ? "border-red-400" : "border-grey-200"}`} 
       />
 
-      {error && (
+      {shouldShowError && (
         <p className="text-red-400 text-[12px] leading-base italic tablet:text-[14px]">
           {error.message}
         </p>
