2ae5b0cdaaf0ef4acf330218d4da717d0081e066
g-akca <gok.akca66@gmail.com>
2026-05-13 22:46:19 +0300

    Add DividerSection content and styles

--- src/components/DividerSection.jsx
+++ src/components/DividerSection.jsx
@@ -1,7 +1,16 @@
+import arrowIcon from "/images/icon-arrow.svg";
+
 function DividerSection() {
   return (
-    <>
-    </>
+    <div className="grid grid-cols-[1fr_auto_1fr] items-center">
+      <div className="h-px bg-grey-200" aria-hidden />
+
+      <button type="submit" className="w-16 aspect-square rounded-full bg-purple-500 flex justify-center items-center">
+        <img src={arrowIcon} alt="Arrow icon" className="w-6 aspect-square" />
+      </button>
+
+      <div className="h-px bg-grey-200" aria-hidden />
+    </div>
   )
 }
 
