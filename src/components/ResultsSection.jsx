4f21aedf9d9977ade1a041d88b6b39863e953443
g-akca <gok.akca66@gmail.com>
2026-05-13 22:59:23 +0300

    Add ResultsSection content and styles

--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -1,7 +1,10 @@
 function ResultsSection() {
   return (
-    <>
-    </>
+    <div className="text-[56px] leading-[110%] tracking-[-2px] font-extrabold italic">
+      <p><span className="text-purple-500">--</span> years</p>
+      <p><span className="text-purple-500">--</span> months</p>
+      <p><span className="text-purple-500">--</span> days</p>
+    </div>
   )
 }
 
