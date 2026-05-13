079501bc069a85796cc97ec61e659b41b370525b
g-akca <gok.akca66@gmail.com>
2026-05-13 18:46:58 +0300

    Change div to main

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,11 +3,11 @@ import ResultsSection from "./ResultsSection";
 
 function MainSection() {
   return (
-    <div className="bg-white px-6 py-12 rounded-3xl rounded-br-[100px] flex flex-col gap-8">
+    <main className="bg-white px-6 py-12 rounded-3xl rounded-br-[100px] flex flex-col gap-8">
       <DateForm />
 
       <ResultsSection />
-    </div>
+    </main>
   )
 }
 
